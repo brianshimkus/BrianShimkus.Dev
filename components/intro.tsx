952a77fc963ee83f1030a2c9f8b3ab832b4d0b03
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-07 10:16:53 -0600

    Framer motion for the intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 export default function Intro() {
 	return (
 		<section>
 			<div className='flex items-center justify-center'>
-				<div>
+				<motion.div
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{
+						type: 'tween',
+						duration: 0.2,
+					}}>
 					<Image
 						src='https://avatars.githubusercontent.com/u/4394337?v=4'
 						alt='Brian portrait'
@@ -14,8 +23,17 @@ export default function Intro() {
 						priority={true}
 						className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
 					/>
-				</div>
+				</motion.div>
 			</div>
+
+			<motion.h1
+				className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}>
+				<span className='font-bold'>Hello, I'm Brian.</span> I'm a{' '}
+				<span className='font-bold'>full-stack developer</span> with{' '}
+				<span className='font-bold'>10 years</span> of experience.
+			</motion.h1>
 		</section>
 	)
 }
