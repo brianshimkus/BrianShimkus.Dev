514188506eb295abf7b67b68d8eb27691ef8c328
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-12 23:46:31 -0600

    Added action buttons section to intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,6 +2,10 @@
 
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa'
 
 export default function Intro() {
 	return (
@@ -34,6 +38,47 @@ export default function Intro() {
 				<span className='font-bold'>full-stack developer</span> with{' '}
 				<span className='font-bold'>10 years</span> of experience.
 			</motion.h1>
+
+			<motion.div
+				className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{
+					delay: 0.1,
+				}}>
+				<Link
+					href='#contact'
+					className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+					onClick={() => {
+						setActiveSection('Contact')
+						setTimeOfLastClick(Date.now())
+					}}>
+					Contact me here{' '}
+					<BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+				</Link>
+
+				<a
+					className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
+					href='/CV.pdf'
+					download>
+					Download Resume{' '}
+					<HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+				</a>
+
+				<a
+					className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+					href='https://linkedin.com'
+					target='_blank'>
+					<BsLinkedin />
+				</a>
+
+				<a
+					className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+					href='https://github.com'
+					target='_blank'>
+					<FaGithubSquare />
+				</a>
+			</motion.div>
 		</section>
 	)
 }
