7c8c18dc648e884ea56349433fce5a79485832ec
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-17 23:40:05 -0600

    Pulled in project data

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,9 +1,22 @@
+import { projectsData } from '@/lib/data'
 import SectionHeading from './section-heading'
+import React from 'react'
 
 export default function Projects() {
 	return (
 		<section>
 			<SectionHeading>My Projects</SectionHeading>
+			<div>
+				{projectsData.map((project, i) => (
+					<React.Fragment key={i}>
+						<Project {...project} />
+					</React.Fragment>
+				))}
+			</div>
 		</section>
 	)
+}
+
+function Project({ title, description, tags, imageUrl }) {
+	return <div>{title}</div>
 }
