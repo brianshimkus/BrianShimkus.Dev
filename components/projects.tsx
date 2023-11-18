71f0ef3d4d374b17d0c0b8325beaf7d761465418
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-17 23:59:44 -0600

    Initial project styling

--- components/projects.tsx
+++ components/projects.tsx
@@ -22,16 +22,25 @@ type ProjectProps = (typeof projectsData)[number]
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
 	return (
-		<section>
-			<h3>{title}</h3>
-			<p>{description}</p>
-			<ul>
+		<section className='bg-gray-800 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8'>
+			<h3 className='text-2xl font-semibold'>{title}</h3>
+			<p className='mt-2 leading-relaxed text-gray-500'>{description}</p>
+			<ul className='flex flex-wrap mt-4 gap-2 relative'>
 				{tags.map((tag, i) => (
-					<li key={i}>{tag}</li>
+					<li
+						className='bg-black/[0.7] px-3 py-1 text-[0.7rem] tracking-wider rounded-full'
+						key={i}>
+						{tag}
+					</li>
 				))}
 			</ul>
 
-			<Image src={imageUrl} alt='Project I worked on' quality={95} />
+			<Image
+				src={imageUrl}
+				alt='Project I worked on'
+				quality={95}
+				className='absolute'
+			/>
 		</section>
 	)
 }
