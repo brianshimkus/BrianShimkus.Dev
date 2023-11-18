436a320b583f5c1012ac9a327cf5d6ad16f9898a
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-17 23:54:50 -0600

    Project scaffold

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import { projectsData } from '@/lib/data'
 import SectionHeading from './section-heading'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Projects() {
 	return (
@@ -17,6 +18,20 @@ export default function Projects() {
 	)
 }
 
-function Project({ title, description, tags, imageUrl }) {
-	return <div>{title}</div>
+type ProjectProps = (typeof projectsData)[number]
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+	return (
+		<section>
+			<h3>{title}</h3>
+			<p>{description}</p>
+			<ul>
+				{tags.map((tag, i) => (
+					<li key={i}>{tag}</li>
+				))}
+			</ul>
+
+			<Image src={imageUrl} alt='Project I worked on' quality={95} />
+		</section>
+	)
 }
