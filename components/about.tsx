6f621393b95a8d598d9f8d471713858bc5d598b0
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-20 01:27:07 -0600

    Custom hook implementation

--- components/about.tsx
+++ components/about.tsx
@@ -1,22 +1,11 @@
 'use client'
 
-import { useInView } from 'react-intersection-observer'
 import SectionHeading from './section-heading'
 import { motion } from 'framer-motion'
-import { useActiveSectionContext } from '@/context/active-section-context'
-import { useEffect } from 'react'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function About() {
-	const { ref, inView } = useInView({
-		threshold: 0.95,
-	})
-	const { setActiveSection } = useActiveSectionContext()
-
-	useEffect(() => {
-		if (inView) {
-			setActiveSection('About')
-		}
-	}, [inView, setActiveSection])
+	const { ref } = useSectionInView('About', 0.95)
 
 	return (
 		<motion.section
--- components/projects.tsx
+++ components/projects.tsx
@@ -2,22 +2,12 @@
 
 import { projectsData } from '@/lib/data'
 import SectionHeading from './section-heading'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Project from './project'
-import { useInView } from 'react-intersection-observer'
-import { useActiveSectionContext } from '@/context/active-section-context'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Projects() {
-	const { ref, inView } = useInView({
-		threshold: 0.95,
-	})
-	const { setActiveSection } = useActiveSectionContext()
-
-	useEffect(() => {
-		if (inView) {
-			setActiveSection('Projects')
-		}
-	}, [inView, setActiveSection])
+	const { ref } = useSectionInView('Projects', 0.95)
 
 	return (
 		<section ref={ref} id='projects' className='scroll-mt-28'>
