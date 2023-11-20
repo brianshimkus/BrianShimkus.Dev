e450b1205ea2c3a19050f74790a20a5317a58938
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-20 01:27:33 -0600

    Get time of last click

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,8 @@ import { motion } from 'framer-motion'
 import Link from 'next/link'
 
 export default function Header() {
-	const { activeSection, setActiveSection } = useActiveSectionContext()
+	const { activeSection, setActiveSection, setTimeOfLastClick } =
+		useActiveSectionContext()
 
 	return (
 		<header className='z-[999] relative'>
@@ -30,7 +31,10 @@ export default function Header() {
 									{ 'text-gray-300': activeSection === link.name }
 								)}
 								href={link.hash}
-								onClick={() => setActiveSection(link.name)}>
+								onClick={() => {
+									setActiveSection(link.name)
+									setTimeOfLastClick(Date.now())
+								}}>
 								{link.name}
 
 								{link.name === activeSection && (
