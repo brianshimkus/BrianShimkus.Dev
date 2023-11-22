14e0d6606ca5c6287cfc8423855618f610cb6668
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-21 23:56:08 -0600

    Initial experience section setup

---
+++ components/experience.tsx
@@ -0,0 +1,9 @@
+import SectionHeading from './section-heading'
+
+export default function Experience() {
+	return (
+		<section id='experience'>
+			<SectionHeading>My Experience</SectionHeading>
+		</section>
+	)
+}
