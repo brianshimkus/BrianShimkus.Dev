5fe86bf46dac144d8a9e48ccfe453793bd065030
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-22 00:20:32 -0600

    Initial experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,9 +1,39 @@
+'use client'
+
+import React from 'react'
 import SectionHeading from './section-heading'
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import { experiencesData } from '@/lib/data'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Experience() {
+	const { ref } = useSectionInView('Experience')
+
 	return (
-		<section id='experience'>
+		<section id='experience' ref={ref} className='scroll-mt-28 mb-28'>
 			<SectionHeading>My Experience</SectionHeading>
+			<VerticalTimeline>
+				{experiencesData.map((item, index) => (
+					<React.Fragment key={index}>
+						<VerticalTimelineElement
+							className='vertical-timeline-element--work'
+							contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+							contentArrowStyle={{
+								borderRight: '7px solid  rgb(33, 150, 243)',
+							}}
+							date={item.date}
+							icon={item.icon}>
+							<h3>{item.title}</h3>
+							<p>{item.location}</p>
+							<p>{item.description}</p>
+						</VerticalTimelineElement>
+					</React.Fragment>
+				))}
+			</VerticalTimeline>
 		</section>
 	)
 }
