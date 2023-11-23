a022d0a8017c13f2db08d12eed0a9ad8f44128e0
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-22 23:40:33 -0600

    Initial contact section scaffold

---
+++ components/contact.tsx
@@ -0,0 +1,9 @@
+import SectionHeading from './section-heading'
+
+export default function Contact() {
+	return (
+		<section id='contact'>
+			<SectionHeading>Contact Me</SectionHeading>
+		</section>
+	)
+}
