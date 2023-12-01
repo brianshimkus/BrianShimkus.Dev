cfd4488fbe53f06bb07f38392edde1fd5ac6f959
Brian Shimkus <brianshimkus1@gmail.com>
2023-12-01 00:04:24 -0600

    Span fix

--- components/intro.tsx
+++ components/intro.tsx
@@ -42,8 +42,10 @@ export default function Intro() {
 				className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
 				initial={{ opacity: 0, y: 100 }}
 				animate={{ opacity: 1, y: 0 }}>
-				<span className='font-bold'>Hello, I'm Brian.</span> I'm a{' '}
-				<span className='font-bold'>full-stack developer</span>.
+				<span className='font-bold'>
+					Hello, I'm Brian. I'm a full-stack developer
+				</span>
+				.
 			</motion.h1>
 
 			<motion.div
