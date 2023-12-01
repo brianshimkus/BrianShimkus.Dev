55fe7b5dc9fd922a9fea16e8b98caba889870b46
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-30 23:55:17 -0600

    Toast notification implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from '@/context/active-section-context'
 import Footer from '@/components/footer'
 import ThemeSwitch from '@/components/theme-switch'
 import ThemeContextProvider from '@/context/theme-context'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,6 +33,7 @@ export default function RootLayout({
 						{children}
 						<Footer />
 
+						<Toaster position='top-right' />
 						<ThemeSwitch />
 					</ActiveSectionContextProvider>
 				</ThemeContextProvider>
